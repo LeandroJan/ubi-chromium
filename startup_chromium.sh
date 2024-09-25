465643039278abb31903347b9870d354f6b472d4
Leandro Jan <nt84h@gmail.com>
2024-09-25 08:33:18 -0300

    added runtime variable VNC_PASSWORD

--- startup_chromium.sh
+++ startup_chromium.sh
@@ -7,10 +7,14 @@ COOKIE=$(mcookie)
 # Create the .Xauthority file with the cookie
 xauth add $DISPLAY . $COOKIE
 
+# Add this line to /home/chromiumuser/startup.sh
+mkdir -p /home/chromiumuser/.vnc && \
+echo "${VNC_PASSWORD}" | vncpasswd -f > /home/chromiumuser/.vnc/passwd
+
 # Start Xvnc server with authentication
 Xvnc $DISPLAY -geometry 1280x800 -depth 24 -SecurityTypes VncAuth -PasswordFile /home/chromiumuser/.vnc/passwd &
 
-sleep 2  # Increased sleep to ensure Xvnc has time to start
+sleep 2  # Ensure Xvnc has time to start
 
 # Set the keyboard layout (e.g., to US English)
 # setxkbmap us
